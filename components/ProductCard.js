f84f6e20cfd1abec09219900b76b0f47537b8815
alxdrian <alex_97piscis@outlook.com>
2022-04-29 09:50:00 -0500

    add styles for product item component

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -5,12 +5,32 @@ function ProductCard(product) {
   } else {
     imageProduct = "./assets/images/NoImageAvailable.png"; 
   }
+  let renderPrice = '';
+  if (product.discount) {
+    let finalPrice = (product.price - (product.discount * product.price / 100)).toFixed(2)/100;
+    renderPrice = `
+      <div class="product-price">
+        <p class="old-price">S/. ${product.price.toFixed(2)/100}</p>
+        <p class="price">S/. ${finalPrice}</p>
+      </div>
+      <div>
+        <p class="discount">- ${product.discount}%</p>
+      </div>
+    `
+  } else {
+    renderPrice = `
+      <div class="product-price">
+        <p class="price">S/. ${product.price.toFixed(2)/100}</p>
+      </div>
+    `
+  }
   return `
     <div class="product-item">
-      <img src="${imageProduct}" alt="${product.name}">
-      <h3>${product.name}</h3>
-      <p>${product.price}</p>
-      <p>${product.discount}</p>
+      <div class="product-image">
+        <img src="${imageProduct}" alt="${product.name}">
+      </div>
+      <h4>${product.name}</h4>
+      ${renderPrice}
     </div>
   `;
 }
