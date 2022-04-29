163af0fccaa9613ef9090d10e44ab57be96a29f4
alxdrian <alex_97piscis@outlook.com>
2022-04-29 12:33:15 -0500

    show the category selected in the navbar

--- scripts/render.js
+++ scripts/render.js
@@ -4,17 +4,26 @@ import { getProductsByFilter } from "../services/productFetch.js";
 import STORE from "./store.js";
 
 export async function renderPage() {
+  // Elimina el mensaje previo de detalles de filtro usado
   const usedFilters = document.querySelector('#used-filters');
   usedFilters.innerHTML = "";
 
+  const selectedTab = document.querySelector('.navbar-item-category.selected');
+  if (selectedTab) {
+    selectedTab.classList.remove('selected');
+  }
+  const newSelectedTab = document.querySelector(`[data-category="${STORE.category.id}"]`);
+  newSelectedTab.classList.add('selected');
+
+   // Obtiene los productos de la API y los muestra en la página
   const productsListContainer = document.querySelector('#products-list-container');
   productsListContainer.innerHTML = '';
-  
   const data = await getProductsByFilter(STORE.name, STORE.category.id);
   data.products.forEach(product => {
     const productCard = ProductCard(product);
     productsListContainer.innerHTML += productCard;
   });
   
+  // Actualiza el mensaje previo de detalles de filtro usado
   usedFilters.innerHTML = UsedFilters({name: STORE.name, category: STORE.category.name});
 }
