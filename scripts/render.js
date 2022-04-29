79a82b8c294e35ecda36414e315059ac03d72f33
alxdrian <alex_97piscis@outlook.com>
2022-04-29 12:56:17 -0500

    add not found message and loader

--- scripts/render.js
+++ scripts/render.js
@@ -7,22 +7,37 @@ export async function renderPage() {
   // Elimina el mensaje previo de detalles de filtro usado
   const usedFilters = document.querySelector('#used-filters');
   usedFilters.innerHTML = "";
+  
+  // Añade un loading mientras cargan los productos
+  const productsListContainer = document.querySelector('#products-list-container');
+  productsListContainer.innerHTML = `
+    <div class="loader"></div>
+  `;
 
-  const selectedTab = document.querySelector('.navbar-item-category.selected');
-  if (selectedTab) {
-    selectedTab.classList.remove('selected');
+  // Elimina la clase que indica la categoria seleccionada previamente
+  const prevSelectedTab = document.querySelector('.navbar-item-category.selected');
+  if (prevSelectedTab) {
+    prevSelectedTab.classList.remove('selected');
   }
+
+  // Agrega la clase que indica la categoria seleccionada actualmente
   const newSelectedTab = document.querySelector(`[data-category="${STORE.category.id}"]`);
   newSelectedTab.classList.add('selected');
 
    // Obtiene los productos de la API y los muestra en la página
-  const productsListContainer = document.querySelector('#products-list-container');
-  productsListContainer.innerHTML = '';
   const data = await getProductsByFilter(STORE.name, STORE.category.id);
-  data.products.forEach(product => {
-    const productCard = ProductCard(product);
-    productsListContainer.innerHTML += productCard;
-  });
+  if (data.products.length > 0) {
+    productsListContainer.innerHTML = "";
+    data.products.forEach(product => {
+      productsListContainer.innerHTML += ProductCard(product);
+    });
+  } else {
+    productsListContainer.innerHTML = `
+      <div class="no-results">
+        <p>No se encontraron resultados</p>
+      </div>
+    `;
+  }
   
   // Actualiza el mensaje previo de detalles de filtro usado
   usedFilters.innerHTML = UsedFilters({name: STORE.name, category: STORE.category.name});
