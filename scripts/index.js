ce9d4465862e041ec9c6c67bcf2a74c602cd6026
alxdrian <alex_97piscis@outlook.com>
2022-04-29 05:13:48 -0500

    get products by database and render them

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,24 @@
-import { getProducts } from "./services/productFetch";
+const BASE_URL = 'http://market-backend-alxdrian.vercel.app/api';
 
-const products = getProducts();
-console.log(products);
+function getProducts() {
+  const response = fetch(`${BASE_URL}/products`).then(res => res.json());
+  return response;
+}
+
+async function renderProducts() {
+  const productsListContainer = document.querySelector('.products-list-container');
+  productsListContainer.innerHTML = '';
+  const data = await getProducts();
+  data.products.forEach(product => {
+    const productItem = document.createElement('div');
+    productItem.classList.add('product-item');
+    productItem.innerHTML = `
+      <img src="${product.url_image}" alt="${product.name}">
+      <h3>${product.name}</h3>
+      <p>${product.price}</p>
+    `;
+    productsListContainer.appendChild(productItem);
+  });
+}
+
+renderProducts();
